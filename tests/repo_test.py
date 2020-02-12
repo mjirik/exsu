bcc8f9cfb54dee17b422ebb186a78313db1c6448
Miroslav Jirik <miroslav.jirik@gmail.com>
2020-02-12 14:36:13 +0100

    better test with non root repo dir

--- tests/repo_test.py
+++ tests/repo_test.py
@@ -9,10 +9,19 @@ from pathlib import Path
 import pandas as pd
 # import openslide
 import exsu.report
-
+import exsu.git_tools
 
 def test_repo():
-    report = exsu.report.Report(repodir=Path(repodir=Path(__file__).parent.parent.resolve()))
+    pth1 = Path(__file__).parent.parent.resolve().absolute()
+    pth2 = Path(__file__).parent.resolve().absolute()
+    logger.debug(f"pth1:{pth1}")
+    logger.debug(f"pth2:{pth2}")
+    rows_git_repo = exsu.git_tools.repo_status_to_dict(repodir=pth1)
+    rows_not_git_repo = exsu.git_tools.repo_status_to_dict(repodir=pth2)
+    assert len(rows_git_repo) > len(rows_not_git_repo)
+
+def test_repo_in_report():
+    report = exsu.report.Report(repodir=Path(__file__).parent.parent.resolve())
     report.finish_actual_row()
     print(report.df)
     assert report.df["repo exsu id"] is not None
