4531fc47f893d2fec026d8ad429b8776bbe64770
Miroslav Jirik <miroslav.jirik@gmail.com>
2020-02-13 21:47:35 +0100

    plf savefig

--- tests/report_test.py
+++ tests/report_test.py
@@ -139,8 +139,12 @@ class ReportTest(unittest.TestCase):
             show=False
         )
         fig = plt.figure()
+        print("before imshow")
         plt.imshow(img)
-        report.savefig_and_show(fn, fig=fig)
+        report.savefig(fn)
+        plt.close()
+        # report.savefig_and_show(fn, fig=fig)
+
         # report.imsave(fn, img, level=60, level_skimage=60, level_npz=60, k=1)
 
         assert (outputdir / fn).exists()
