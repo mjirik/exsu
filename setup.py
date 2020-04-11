a84b6cccd99e6accbbb17abafdd84948add6dbf8
Miroslav Jirik <miroslav.jirik@gmail.com>
2020-04-11 12:17:09 +0200

    Bump version: 0.3.7 → 0.3.8

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # http://packaging.python.org/en/latest/tutorial.html#version
-    version='0.3.7',
+    version='0.3.8',
     url='https://github.com/mjirik/exsu',
     author='Miroslav Jirik',
     author_email='miroslav.jirik@gmail.com',
