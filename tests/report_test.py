66263d962e1a97a299b6dac7ef909d0624911ed8
Miroslav Jiřík <miroslav.jirik@gmail.com>
2021-03-29 09:04:28 +0200

    fixed read xlsx by openpyxl

--- tests/report_test.py
+++ tests/report_test.py
@@ -17,108 +17,111 @@ import pytest
 pytest_plugins = "pytester"
 
 
-class ReportTest(unittest.TestCase):
+# class ReportTest(unittest.TestCase):
 
-    skip_on_local = False
+    # skip_on_local = False
     # skip_on_local = True
     # @unittest.skipIf(os.environ.get("TRAVIS", skip_on_local), "Skip on Travis-CI")
-    def test_save_report_excel(self):
-        outputdir = Path("./test_report/")
-        # if outputdir.exists():
-        #     shutil.rmtree(outputdir)
-        commonsheet = Path("./test_report_common_spreadsheet.xlsx")
-        if commonsheet.exists():
-            os.remove(commonsheet)
+def test_save_report_excel():
+    outputdir = Path("./test_report/")
+    # if outputdir.exists():
+    #     shutil.rmtree(outputdir)
+    commonsheet = Path("./test_report_common_spreadsheet.xlsx")
+    if commonsheet.exists():
+        os.remove(commonsheet)
 
-        report = exsu.report.Report(
-            outputdir=outputdir, additional_spreadsheet_fn=commonsheet
-        )
-        report.add_cols_to_actual_row({"Col1": 25, "Col2": "test string", "Col5": 5})
-        report.add_cols_to_actual_row(
-            {"Col2": "prepsanu", "Col1": 26, "Col4": "ctyrka"}
-        )
-        report.finish_actual_row()
+    report = exsu.report.Report(
+        outputdir=outputdir, additional_spreadsheet_fn=commonsheet
+    )
+    report.add_cols_to_actual_row({"Col1": 25, "Col2": "test string", "Col5": 5})
+    report.add_cols_to_actual_row(
+        {"Col2": "prepsanu", "Col1": 26, "Col4": "ctyrka"}
+    )
+    report.finish_actual_row()
 
-        report.add_cols_to_actual_row(
-            {"Col1": 27, "Col2": "test string", "Col3": "trojka"}
-        )
-        report.finish_actual_row()
-        report.dump()
+    report.add_cols_to_actual_row(
+        {"Col1": 27, "Col2": "test string", "Col3": "trojka"}
+    )
+    report.finish_actual_row()
+    report.dump()
 
-        # new write to common excel
-        report.init()
-        report.add_cols_to_actual_row(
-            {"Col1": 28, "Col2": "new line to common", "Col7": 77}
-        )
-        report.finish_actual_row()
-        report.dump()
+    # new write to common excel
+    report.init()
+    report.add_cols_to_actual_row(
+        {"Col1": 28, "Col2": "new line to common", "Col7": 77}
+    )
+    report.finish_actual_row()
+    report.dump()
 
-        df = pd.read_excel(commonsheet)
-        self.assertEqual(len(df), 3, "3 lines expected in the excel file")
+    df = pd.read_excel(commonsheet,
+                       engine="openpyxl"
+                       )
+    assert len(df) == 3, "3 lines expected in the excel file"
 
-    # def test_imsave_and_report_to_various_spreadsheets(self):
-    #     commonsheet0 = Path("./test_report_common_spreadsheet.xls")
-    #     commonsheet1 = Path("./test_report_common_spreadsheet.xlsx")
-    #     commonsheet2 = Path("./test_report_common_spreadsheet.csv")
-    #     self.imsave_and_report_to(commonsheet0)
-    #     self.imsave_and_report_to(commonsheet1)
-    #     self.imsave_and_report_to(commonsheet2)
+# def test_imsave_and_report_to_various_spreadsheets(self):
+#     commonsheet0 = Path("./test_report_common_spreadsheet.xls")
+#     commonsheet1 = Path("./test_report_common_spreadsheet.xlsx")
+#     commonsheet2 = Path("./test_report_common_spreadsheet.csv")
+#     self.imsave_and_report_to(commonsheet0)
+#     self.imsave_and_report_to(commonsheet1)
+#     self.imsave_and_report_to(commonsheet2)
 
-    def test_imsave_npz_and_skimage(self):
-        outputdir = Path("./test_report/")
-        commonsheet = Path("./test_report_common_spreadsheet.xlsx")
-        if commonsheet.exists():
-            os.remove(commonsheet)
-        fn = "test_image.png"
-        fn_npz = Path("test_image.npz")
-        fn_skimage = Path("test_image_skimage.png")
-        if Path(fn).exists():
-            os.remove(fn)
-        if fn_npz.exists():
-            os.remove(fn_npz)
-        if fn_skimage.exists():
-            os.remove(fn_skimage)
+def test_imsave_npz_and_skimage():
+    outputdir = Path("./test_report/")
+    commonsheet = Path("./test_report_common_spreadsheet.xlsx")
+    if commonsheet.exists():
+        os.remove(commonsheet)
+    fn = "test_image.png"
+    fn_npz = Path("test_image.npz")
+    fn_skimage = Path("test_image_skimage.png")
+    if Path(fn).exists():
+        os.remove(fn)
+    if fn_npz.exists():
+        os.remove(fn_npz)
+    if fn_skimage.exists():
+        os.remove(fn_skimage)
 
-        img = 50 + np.random.rand(100, 100) * 30
-        img[20:60, 20:60] += 100
-        img = img.astype(np.uint8)
-        report = exsu.report.Report(
-            outputdir=outputdir, additional_spreadsheet_fn=commonsheet, level=50
-        )
-        report.imsave(fn, img, level=60, level_skimage=60, level_npz=60, k=1)
+    img = 50 + np.random.rand(100, 100) * 30
+    img[20:60, 20:60] += 100
+    img = img.astype(np.uint8)
+    report = exsu.report.Report(
+        outputdir=outputdir, additional_spreadsheet_fn=commonsheet, level=50
+    )
+    report.imsave(fn, img, level=60, level_skimage=60, level_npz=60, k=1)
 
-        assert (outputdir / fn).exists()
-        assert (outputdir / fn_npz).exists()
-        assert (outputdir / fn_skimage).exists()
+    assert (outputdir / fn).exists()
+    assert (outputdir / fn_npz).exists()
+    assert (outputdir / fn_skimage).exists()
 
-    def test_imsave_npz_and_skimage_color(self):
-        outputdir = Path("./test_report/")
-        commonsheet = Path("./test_report_common_spreadsheet.xlsx")
-        if commonsheet.exists():
-            os.remove(commonsheet)
-        fn = "test_image_color.png"
-        fn_npz = Path("test_image_color.npz")
-        fn_skimage = Path("test_image_color_skimage.png")
-        if Path(fn).exists():
-            os.remove(fn)
-        if fn_npz.exists():
-            os.remove(fn_npz)
-        if fn_skimage.exists():
-            os.remove(fn_skimage)
 
-        img = 50 + np.random.rand(100, 100, 3) * 30
-        img[20:60, 20:60, 0] += 100
-        img[40:80, 20:60, 1] += 100
-        img[40:60, 40:80, 2] += 100
-        img = img.astype(np.uint8)
-        report = exsu.report.Report(
-            outputdir=outputdir, additional_spreadsheet_fn=commonsheet, level=50
-        )
-        report.imsave(fn, img, level=60, level_skimage=60, level_npz=60, k=1)
+def test_imsave_npz_and_skimage_color():
+    outputdir = Path("./test_report/")
+    commonsheet = Path("./test_report_common_spreadsheet.xlsx")
+    if commonsheet.exists():
+        os.remove(commonsheet)
+    fn = "test_image_color.png"
+    fn_npz = Path("test_image_color.npz")
+    fn_skimage = Path("test_image_color_skimage.png")
+    if Path(fn).exists():
+        os.remove(fn)
+    if fn_npz.exists():
+        os.remove(fn_npz)
+    if fn_skimage.exists():
+        os.remove(fn_skimage)
 
-        assert (outputdir / fn).exists()
-        assert (outputdir / fn_npz).exists()
-        assert (outputdir / fn_skimage).exists()
+    img = 50 + np.random.rand(100, 100, 3) * 30
+    img[20:60, 20:60, 0] += 100
+    img[40:80, 20:60, 1] += 100
+    img[40:60, 40:80, 2] += 100
+    img = img.astype(np.uint8)
+    report = exsu.report.Report(
+        outputdir=outputdir, additional_spreadsheet_fn=commonsheet, level=50
+    )
+    report.imsave(fn, img, level=60, level_skimage=60, level_npz=60, k=1)
+
+    assert (outputdir / fn).exists()
+    assert (outputdir / fn_npz).exists()
+    assert (outputdir / fn_skimage).exists()
 
 
 def test_report_no_outputdir(tmp_path):
@@ -141,7 +144,9 @@ def test_report_no_outputdir(tmp_path):
     report.finish_actual_row()
     report.dump()
     assert commonsheet.exists()
-    dfo = pd.read_excel(commonsheet)
+    dfo = pd.read_excel(commonsheet,
+                        engine="openpyxl"
+                        )
     assert "numpy_version" in dfo.keys()
 
 
